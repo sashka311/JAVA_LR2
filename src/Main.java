1625e30073bf1991c085dce0f338dbfe6d5600b7
sashka311 <ihor.garashchenko@gmail.com>
2022-09-22 20:21:44 +0300

    first version

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This task done by Ihor Garashchenko SE-19-1/9");
+    }
+}
