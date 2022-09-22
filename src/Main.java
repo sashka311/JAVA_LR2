e06160517e334b15015251572de3a36aa45e7946
sashka311 <ihor.garashchenko@gmail.com>
2022-09-22 20:25:42 +0300

    add random generator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This task done by Ihor Garashchenko SE-19-1/9");
+        Random rand = new Random();
+        int n = rand.nextInt(50);
+        System.out.println("Random number [1-50] = "+ n);
     }
 }
