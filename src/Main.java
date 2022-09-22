9488685f90a0ad3b1670d95c60794026bc7fcc79
sashka311 <108427409+sashka311@users.noreply.github.com>
2022-09-22 20:30:44 +0300

    update main java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        //some browser changes
         System.out.println("Hello world!");
         System.out.println("This task done by Ihor Garashchenko SE-19-1/9");
         Date date = new Date();
